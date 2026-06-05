5a05029943d430b9006c9a8d0f1b11505f99dcd6
Radha Krishna <radhakrishna@Mac.lan>
2026-06-05 07:53:53 +0530

    Allow frontend deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,9 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:4200"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
