74ace065cc31228d04cfa129c5ed3a69e7de3908
Radha Krishna <radhakrishna@Mac.lan>
2026-06-07 16:00:37 +0530

    Changed to better error handaling

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
 from app.routes.auth import router as authRouter
 from app.database import Base, engine
 from app.models.recruiter import Lead
@@ -22,9 +24,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(
+    request: Request,
+    exc: HTTPException
+):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={
+            "success": False,
+            "message": exc.detail
+        }
+    )
+
+
+@app.exception_handler(Exception)
+async def general_exception_handler(
+    request: Request,
+    exc: Exception
+):
+    print(f"Unhandled Error: {exc}")
+
+    return JSONResponse(
+        status_code=500,
+        content={
+            "success": False,
+            "message": "An unexpected error occurred"
+        }
+    )
+
+
 app.include_router(recruiter_router)
 app.include_router(authRouter)
 
+
 @app.get("/")
 def home():
-    return {"message": "Recruiter CRM Backend Running Successfully"}
+    return {
+        "message": "Recruiter CRM Backend Running Successfully"
+    }
