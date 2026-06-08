fe158e0f5a664ddd2266a0c3b5180208251ae790
Radha Krishna <radhakrishna@Mac.lan>
2026-06-08 16:00:45 +0530

    test loading enimation

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -1,19 +1,10 @@
-import {
-  Injectable,
-  inject,
-  signal
-} from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 
 import { tap } from 'rxjs/operators';
 
-import {
-  LoginRequest,
-  SignupRequest,
-  AuthResponse,
-  CurrentUser
-} from '../models/user.model';
+import { LoginRequest, SignupRequest, AuthResponse, CurrentUser } from '../models/user.model';
 import { Storage } from './storage';
 import { Router } from '@angular/router';
 
@@ -21,166 +12,91 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class Auth {
-
   private http = inject(HttpClient);
   private router = inject(Router);
   private storage = inject(Storage);
 
-  private apiUrl =
-  `${environment.apiBaseUrl}/auth`;
+  private apiUrl = `${environment.apiBaseUrl}/auth`;
 
   isAuthenticated = signal(false);
 
-  currentUser = signal<CurrentUser | null>(
-    null
-  );
+  currentUser = signal<CurrentUser | null>(null);
 
   constructor() {
+    const loggedIn = this.storage.isLoggedIn();
 
-    const loggedIn =
-      this.storage.isLoggedIn();
-
-    this.isAuthenticated.set(
-      loggedIn
-    );
+    this.isAuthenticated.set(loggedIn);
 
     if (!loggedIn) {
-
       this.storage.clearSession();
 
       return;
-
     }
 
-    this.getCurrentUser()
-      .subscribe({
-
-        error: () => {
-
-          this.logout();
-
-        }
-
-      });
-
+    this.getCurrentUser().subscribe({
+      error: () => {
+        this.logout();
+      },
+    });
   }
 
-  signup(
-    user: SignupRequest,
-    callback?: () => void
-  ) {
-
-    this.http.post(
-      `${this.apiUrl}/signup`,
-      user
-    ).subscribe({
-
+  signup(user: SignupRequest, callback?: () => void) {
+    this.http.post(`${this.apiUrl}/signup`, user).subscribe({
       next: () => {
-
         if (callback) {
           callback();
         }
-
       },
 
       error: (error) => {
-        console.error(
-          'Signup failed:',
-          error
-        );
-      }
-
+        console.error('Signup failed:', error);
+      },
     });
-
   }
 
-  login(
-    user: LoginRequest,
-    callback?: () => void
-  ) {
-
-    this.http.post<AuthResponse>(
-      `${this.apiUrl}/login`,
-      user
-    ).subscribe({
-
+  login(user: LoginRequest, successCallback?: () => void, errorCallback?: () => void) {
+    this.http.post<AuthResponse>(`${this.apiUrl}/login`, user).subscribe({
       next: (response) => {
+        this.storage.saveToken(response.access_token);
 
-        this.storage.saveToken(
-          response.access_token
-        );
+        this.isAuthenticated.set(true);
 
-        this.isAuthenticated.set(
-          true
-        );
-
-        this.getCurrentUser()
-          .subscribe({
-
-            next: () => {
-
-              if (callback) {
-                callback();
-              }
-
-            },
-
-            error: (error) => {
-
-              console.error(
-                'Failed to load user:',
-                error
-              );
-
+        this.getCurrentUser().subscribe({
+          next: () => {
+            if (successCallback) {
+              successCallback();
             }
+          },
 
-          });
+          error: (error) => {
+            console.error('Login failed:', error);
 
+            if (errorCallback) {
+              errorCallback();
+            }
+          },
+        });
       },
 
       error: (error) => {
-
-        console.error(
-          'Login failed:',
-          error
-        );
-
-      }
-
+        console.error('Login failed:', error);
+      },
     });
-
   }
 
   getCurrentUser() {
-
-    return this.http
-      .get<CurrentUser>(
-        `${this.apiUrl}/me`
-      )
-      .pipe(
-
-        tap(user => {
-
-          this.currentUser.set(
-            user
-          );
-
-        })
-
-      );
-
+    return this.http.get<CurrentUser>(`${this.apiUrl}/me`).pipe(
+      tap((user) => {
+        this.currentUser.set(user);
+      }),
+    );
   }
 
   logout() {
+    this.storage.removeToken();
 
-  this.storage.removeToken();
+    this.isAuthenticated.set(false);
 
-  this.isAuthenticated.set(false);
-
-  this.router.navigate([
-    '/login'
-  ]);
-
-}
-
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { Auth } from "../../../core/services/auth";
+import { Auth } from '../../../core/services/auth';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { Router, RouterLink } from '@angular/router';
@@ -9,7 +9,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { MatInputModule } from '@angular/material/input';
-
+import { signal } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
 
 @Component({
@@ -22,45 +23,47 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule,
   ],
 
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
-
   private fb = inject(FormBuilder);
   private router = inject(Router);
-  private auth = inject(Auth)
+  private auth = inject(Auth);
+  isLoading = signal(false);
 
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
 
-    password: ['', [Validators.required, Validators.minLength(6)]]
+    password: ['', [Validators.required, Validators.minLength(6)]],
   });
 
   onSubmit() {
-
-  if (this.loginForm.invalid) {
-    return;
-  }
-
-  this.auth.login(
-    {
-      email: this.loginForm.value.email!,
-      password: this.loginForm.value.password!
-    },
-
-    () => {
-
-      this.router.navigate([
-        '/dashboard'
-      ]);
-
+    if (this.loginForm.invalid || this.isLoading()) {
+      return;
     }
-  );
 
-}
+    this.isLoading.set(true);
 
+    this.auth.login(
+      {
+        email: this.loginForm.value.email!,
+        password: this.loginForm.value.password!,
+      },
+
+      () => {
+        this.isLoading.set(false);
+
+        this.router.navigate(['/dashboard']);
+      },
+
+      () => {
+        this.isLoading.set(false);
+      },
+    );
+  }
 }
